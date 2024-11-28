82e345376a6e9154ae528a75c8552fc18657a54e
Charlie <charlie.elizarbe@unmsm.edu.pe>
2024-11-28 15:15:26 -0500

    Agregar archivo index.html y configurar rutas para la API en app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import express from 'express'
 import cors from 'cors'
 import routes from './routes'
+import path from 'path'
 
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Welcome to the API! This is a test message for the /api route.' })
+})
 
 app.use('/api', routes)
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'))
+})
 
 export default app
