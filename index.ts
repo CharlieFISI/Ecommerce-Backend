7f869c58431b5b76cfb95585a845352c0f24cd9a
Charlie <charlie.elizarbe@unmsm.edu.pe>
2024-11-28 16:07:11 -0500

    Mejorar manejo de servidor y rutas: agregar cierre del servidor en SIGTERM, actualizar script de construcción y ajustar rutas en app.ts

--- index.ts
+++ index.ts
@@ -2,10 +2,15 @@ import app from './src/app'
 
 const PORT = process.env.PORT
 
-if (process.env.NODE_ENV !== 'production') {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+const server = app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server')
+  server.close(() => {
+    console.log('HTTP server closed')
   })
-}
+})
 
 export default app
--- src/app.ts
+++ src/app.ts
@@ -14,8 +14,8 @@ app.get('/api', (req, res) => {
 })
 
 app.use('/api', routes)
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public', 'index.html'))
 })
 
 export default app
