5f9e856e3963863033c0e271dbb17bb3ae95e098
Charlie <charlie.elizarbe@unmsm.edu.pe>
2024-11-28 21:00:11 -0500

    Actualizar la configuración de Prisma para usar MONGODB_URI en lugar de DATABASE_URL

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -4,9 +4,10 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       NODE_ENV: 'development' | 'production' | 'test'
-      PORT: string
       DATABASE_URL: string
       JWT_SECRET: string
+      MONGODB_URI: string
+      PORT: string
     }
   }
 }
