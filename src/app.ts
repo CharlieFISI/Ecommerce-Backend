4302a17d8dd5fad7a1e651d96160c5f02073d257
Charlie <charlie.elizarbe@unmsm.edu.pe>
2024-12-08 23:37:49 -0500

    Actualizar configuración de CORS para permitir cualquier origen y simplificar el manejo de orígenes permitidos

--- src/app.ts
+++ src/app.ts
@@ -6,24 +6,34 @@ import path from 'path'
 const app = express()
 app.use(express.json())
 
-const allowedOrigins = [
-  process.env.DASHBOARD_FRONTEND_URL,
-  process.env.LOCAL_MOBILE_FRONTEND_URL
-].filter(Boolean)
+// const allowedOrigins = [
+//   process.env.DASHBOARD_FRONTEND_URL,
+//   process.env.LOCAL_MOBILE_FRONTEND_URL,
+//   process.env.OTHER_MOBILE_FRONTEND_URL,
+//   process.env.MOBILE_FRONTEND_URL
+// ].filter(Boolean)
 
-const corsOptions: CorsOptions = {
-  origin: function (
-    origin: string | undefined,
-    callback: (error: Error | null, allow?: boolean) => void
-  ) {
-    if (origin === undefined) {
-      callback(null, true)
-    } else if (allowedOrigins.includes(origin)) {
-      callback(null, true)
-    } else {
-      callback(new Error('Not allowed by CORS'), false)
-    }
-  },
+// const corsOptions: CorsOptions = {
+//   origin: function (
+//     origin: string | undefined,
+//     callback: (error: Error | null, allow?: boolean) => void
+//   ) {
+//     if (origin === undefined) {
+//       callback(null, true)
+//     } else if (allowedOrigins.includes(origin)) {
+//       callback(null, true)
+//     } else {
+//       callback(new Error('Not allowed by CORS'), false)
+//     }
+//   },
+//   credentials: true,
+//   optionsSuccessStatus: 200
+// }
+
+const corsOptions: cors.CorsOptions = {
+  origin: '*', // Esto permite cualquier origen
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
   optionsSuccessStatus: 200
 }
@@ -44,6 +54,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public', 'index.html'))
 })
 
-console.log('Allowed CORS origins:', allowedOrigins)
+// console.log('Allowed CORS origins:', allowedOrigins)
 
 export default app
--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -9,6 +9,7 @@ declare global {
       LOCAL_DASHBOARD_FRONTEND_URL: string
       DASHBOARD_FRONTEND_URL: string
       LOCAL_MOBILE_FRONTEND_URL: string
+      OTHER_MOBILE_FRONTEND_URL: string
       MOBILE_FRONTEND_URL: string
       MONGODB_URI: string
       PORT: string
